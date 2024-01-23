a225ded211b5691266b12584a8968665ba85ea5d
JesusdelCas99 <grjesueeweawes99@gmail.com>
2024-01-23 01:48:05 +0100

    Update client JavaScript

--- src/static/main.js
+++ src/static/main.js
@@ -1,27 +1,24 @@
-// Client-Server connections
-// ---------------------------------------------------------
-// Establish WebSocket connection to 'Flask' server on 127.0.0.1:5000
+/* Client "main.js" JavaScript file */
+
+/* (Client-Server connections) Establish WebSocket connection to the server */
 const socket = io.connect('http://127.0.0.1:5000');
 
 
-// Client Events
-// ---------------------------------------------------------
-// Client 'onclick' WebSocket event 'check_status_request'
+/* (Client event) Client 'onclick' WebSocket event 'check_status_request' */
 function WebSocketsSendRequest() {
     const mensaje = document.getElementById('requestType').value;
     socket.emit('check_status_request', mensaje);
 }
 
 
-// Server Events
-// ---------------------------------------------------------
+/* Server Events */
 document.addEventListener('DOMContentLoaded', function () {
-    // Server WebSockets event 'check_status_response'
+    /* (Server event) Server WebSockets event 'check_status_response' */
     handleServerStatusEvent();
 });
 
 
-// Server WebSockets event 'check_status_response'
+/* Server WebSockets event 'check_status_response' */
 function handleServerStatusEvent() {
     const timestamp = new Date().toLocaleTimeString();
     socket.on('check_status_response', (data) => {
