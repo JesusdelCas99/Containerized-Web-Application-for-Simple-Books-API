7768ec4ce06bdabe2588dce4e7ad17e42257b6a6
JesusdelCas99 <grjesueeweawes99@gmail.com>
2024-01-28 01:29:25 +0100

    Update project

--- src/server.py
+++ src/server.py
@@ -15,7 +15,25 @@ This route directs users to 'index.html' page
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+
+'''
+Route ('/status'): 
+Returns API Status                        
+'''
+@app.route('/status')
+def status():
+    return render_template('status.html')
+
+
+'''
+Route ('/bookList'): 
+Returns a list of books.                
+'''
+@app.route('/bookList')
+def bookList():
+    return render_template('bookList.html')
+
 
 """
 Server-side event handler for a Socket.IO connection:
@@ -40,6 +58,35 @@ def handle_check_for_connection(data):
     else:
         # If the request was unsuccessful, return an error message to the client
         socketio.emit('check_status_response', "'error': 'Failed to fetch data from the API'")
+
+
+"""
+Server-side event handler for a Socket.IO connection:
+* Server listens for 'bookList_request' WebSocket event
+* Server emits 'bookList_response' WebSocket event               
+"""
+@socketio.on('bookList_request')
+def handle_bookList(typeValue, limitValue):
+    # Verify connection
+    print(f"Received 'bookList_request' event with data: Type - {typeValue}, Limit - {limitValue}")
+
+    # Specify headers for the request
+    headers = {'Content-Type': 'application/json'}
+
+    # Construct the parameters for the API request
+    params = {'type': typeValue, 'limit': limitValue}
+
+    # Make a GET request to the external API with the specified parameters
+    api_url = 'https://simple-books-api.glitch.me/books'
+    response = requests.get(api_url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        # Submit JSON response
+        socketio.emit('bookList_response', json.loads(response.content))
+    else:
+        # If the request was unsuccessful, return an error message to the client
+        socketio.emit('bookList_response', {'error': 'Failed to fetch data from the API'})
+
 
 
 # Run application in debug mode
--- src/static/main.js
+++ src/static/main.js
@@ -3,11 +3,43 @@
 /* (Client-Server connections) Establish WebSocket connection to the server */
 const socket = io.connect('http://127.0.0.1:5000');
 
+/* (Client event) Client 'onclick' event "Clear response"*/
+function ClientClearResponse() {
+    document.getElementById('result').innerHTML = '';
+}
+
 
 /* (Client event) Client 'onclick' WebSocket event 'check_status_request' */
-function WebSocketsSendRequest() {
-    const mensaje = document.getElementById('requestType').value;
-    socket.emit('check_status_request', mensaje);
+function WebSocketsStatusRequest() {
+    socket.emit('check_status_request', "dummy text");
+}
+
+/* (Client event) Client 'onclick' WebSocket event 'bookList_request' */
+function WebSocketsbookListRequest() {
+    var typeValue = document.getElementById("typeInput").value;
+    var limitValue = document.getElementById("limitInput").value;
+
+    // Check for input typeValue content
+    if (typeValue !== "") {
+        if (typeValue.toLowerCase() !== "fiction" && typeValue.toLowerCase() !== "non-fiction"){
+            alert("Please enter a correct value for Type.");
+            return;
+        }
+    }
+
+    // Check if the limit value is a valid integer
+    if (limitValue !== "") {
+        if (!(/^\d+$/.test(limitValue))) {
+            alert("Please enter a correct value for Type.");
+            return;
+        }
+        else if (parseInt(limitValue) < 1 || parseInt(limitValue) > 20 ){
+            alert("Please enter an integer value for Limit.");
+            return;
+        }
+    }
+
+    socket.emit('bookList_request', typeValue, limitValue);
 }
 
 
@@ -15,13 +47,51 @@ function WebSocketsSendRequest() {
 document.addEventListener('DOMContentLoaded', function () {
     /* (Server event) Server WebSockets event 'check_status_response' */
     handleServerStatusEvent();
+    handleServerBookListEvent();
 });
 
 
 /* Server WebSockets event 'check_status_response' */
 function handleServerStatusEvent() {
-    const timestamp = new Date().toLocaleTimeString();
     socket.on('check_status_response', (data) => {
-        document.getElementById('result').innerHTML += `${timestamp} - (simple-books-api) Status: ${data}<br>`;
+        let now = new Date();
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let day = now.getDate();
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        let second = now.getSeconds();
+
+        document.getElementById('result').innerHTML = `(${day}/${month}/${year}) - ${hour*3600 + 60*minute + second}'' - (simple-books-api) Status: ${data}<br>`;
     });
+}
+
+function handleServerBookListEvent() {
+    socket.on('bookList_response', (data) => {
+        // Parse the JSON data
+        var books = data
+
+        // Initialize an empty string to store the HTML markup
+        var html = '';
+
+        // Iterate over each book object
+        books.forEach(function(book) {
+            // Generate HTML markup for each book
+            html += '<div>';
+            html += '<hr>'
+            html += '<p><strong>ID:</strong> ' + book.id + '</p>';
+            html += '<p><strong>Name:</strong> ' + book.name + '</p>';
+            html += '<p><strong>Type:</strong> ' + book.type + '</p>';
+            html += '<p><strong>Available:</strong> ' + (book.available ? 'Yes' : 'No') + '</p>';
+            html += '</div>';
+        });
+
+        // Set the HTML content of the 'result' element to display the generated HTML
+        document.getElementById('result').innerHTML = html;
+    });
+}
+
+// JavaScript function to redirect to a specific page
+function redirectToPage(url) {
+    window.location.href = url;
 }
