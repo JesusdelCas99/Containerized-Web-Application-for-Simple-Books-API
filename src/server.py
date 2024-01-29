0db7a252c46dacad4ceffae12841081fd17700da
JesusdelCas99 <grjesueeweawes99@gmail.com>
2024-01-29 13:31:55 +0100

    Update project

--- src/server.py
+++ src/server.py
@@ -8,47 +8,44 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 
 
-'''
-Default route ('/'): 
-This route directs users to 'index.html' page                        
-'''
+# Default route ('/')
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-'''
-Route ('/status'): 
-Returns API Status                        
-'''
+# Route ('/status'): Returns API Status                        
 @app.route('/status')
 def status():
     return render_template('status.html')
 
 
-'''
-Route ('/bookList'): 
-Returns a list of books.                
-'''
+# Route ('/bookList'): Returns a list of books
 @app.route('/bookList')
 def bookList():
     return render_template('bookList.html')
 
 
+# Route ('/bookInfo'): Returns detailed information about a book
+@app.route('/bookInfo')
+def bookInfo():
+    return render_template('bookInfo.html')
+
+
 """
 Server-side event handler for a Socket.IO connection:
-* Server listens for 'check_status_request' WebSocket event
-* Server emits 'check_status_response' WebSocket event               
+* Server listens for 'check_status_request' webSocket event
+* Server emits 'check_status_response' webSocket event               
 """
 @socketio.on('check_status_request')
-def handle_check_for_connection(data):
+def handle_check_for_connection():
     # Verify connection
-    print(f"Received 'check_for_connection' event with data: {data}")
+    print(f"Received 'check_for_connection' event")
 
     # Specify headers for the request
     headers = {'Content-Type': 'application/json'}
 
-    # Make a GET request to the external API
+    # Make a GET request to external API
     api_url = 'https://simple-books-api.glitch.me/status'
     response = requests.get(api_url, headers=headers)
 
@@ -62,13 +59,13 @@ def handle_check_for_connection(data):
 
 """
 Server-side event handler for a Socket.IO connection:
-* Server listens for 'bookList_request' WebSocket event
-* Server emits 'bookList_response' WebSocket event               
+* Server listens for 'bookList_request' webSocket event
+* Server emits 'bookList_response' webSocket event               
 """
 @socketio.on('bookList_request')
 def handle_bookList(typeValue, limitValue):
     # Verify connection
-    print(f"Received 'bookList_request' event with data: Type - {typeValue}, Limit - {limitValue}")
+    print(f"Received bookList_request event with data: Type - {typeValue}, Limit - {limitValue}")
 
     # Specify headers for the request
     headers = {'Content-Type': 'application/json'}
@@ -76,7 +73,7 @@ def handle_bookList(typeValue, limitValue):
     # Construct the parameters for the API request
     params = {'type': typeValue, 'limit': limitValue}
 
-    # Make a GET request to the external API with the specified parameters
+    # Make a GET request to external API with the specified parameters
     api_url = 'https://simple-books-api.glitch.me/books'
     response = requests.get(api_url, headers=headers, params=params)
 
@@ -86,6 +83,32 @@ def handle_bookList(typeValue, limitValue):
     else:
         # If the request was unsuccessful, return an error message to the client
         socketio.emit('bookList_response', {'error': 'Failed to fetch data from the API'})
+
+
+"""
+Server-side event handler for a Socket.IO connection:
+* Server listens for 'bookInfo_request' webSocket event
+* Server emits 'bookInfo_response' webSocket event               
+"""
+@socketio.on('bookInfo_request')
+def handle_bookInfo(bookId):
+    # Verify connection
+    print(f"Received bookInfo_request event with data: bookdId - {bookId}")
+
+    # Specify headers for the request
+    headers = {'Content-Type': 'application/json'}
+
+    # Make a GET request to external API with the specified parameters
+    api_url = 'https://simple-books-api.glitch.me/books' + f'/{bookId}'
+    print(api_url)
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        # Submit JSON response
+        socketio.emit('bookInfo_response', json.loads(response.content))
+    else:
+        # If the request was unsuccessful, return an error message to the client
+        socketio.emit('bookInfo_response', {'error': 'Failed to fetch data from the API'})
 
 
 
--- src/static/main.js
+++ src/static/main.js
@@ -16,7 +16,29 @@ function redirectToPage(url) {
 
 /* (Client event) Client 'onclick' webSocket event 'check_status_request' */
 function WebSocketsStatusRequest() {
-    socket.emit('check_status_request', "dummy text");
+    socket.emit('check_status_request');
+}
+
+/* (Client event) Client 'onclick' webSocket event 'bookInfo_request' */
+function WebSocketsbookInfoRequest(){
+    var bookId = document.getElementById("typeInput").value;
+
+    // Check if the limit value is a valid integer
+    if (bookId === ""){
+        alert("Please enter a correct value for Book id.");
+        return;
+    }
+    else if (bookId !== "") {
+        if (!(/^\d+$/.test(bookId))) {
+            alert("Please enter a correct value for Book id.");
+            return;
+        }
+        else if (parseInt(bookId) < 1 || parseInt(bookId) > 6 ){
+            alert("Please enter an integer value (0 < book id < 7) for book id.");
+            return;
+        }
+    }
+    socket.emit('bookInfo_request', bookId);
 }
 
 /* (Client event) Client 'onclick' webSocket event 'bookList_request' */
@@ -53,7 +75,38 @@ document.addEventListener('DOMContentLoaded', function () {
     handleServerStatusEvent();
     /* (Server event) Server webSocket event 'bookList_response' */
     handleServerBookListEvent();
+    /* (Server event) Server webSocket event 'bookInfo_response' */
+    handleServerBookInfoEvent();
 });
+
+
+/* (Server event) Server webSocket event 'bookInfo_response' */
+function handleServerBookInfoEvent() {
+    socket.on('bookInfo_response', (data) => {
+
+        // Parse the JSON data
+        var book = data;
+
+        // Initialize an empty string to store the HTML markup
+        var html = '';
+
+        // Generate HTML markup for each book
+        html += '<div>';
+        html += '<hr>';
+        html += '<p><strong>ID:</strong> ' + book.id + '</p>';
+        html += '<p><strong>Name:</strong> ' + book.name + '</p>'; // Removed extra '+' sign
+        html += '<p><strong>Author:</strong> ' + book.author + '</p>';
+        html += '<p><strong>ISBN:</strong> ' + book.isbn + '</p>';
+        html += '<p><strong>Type:</strong> ' + book.type + '</p>';
+        html += '<p><strong>Price:</strong> ' + book.price + '</p>';
+        html += '<p><strong>Available:</strong> ' + (book.available ? 'Yes' : 'No') + '</p>';
+        html += '</div>';
+
+        // Set the HTML content of the 'result' element to display the generated HTML
+        document.getElementById('result').innerHTML = html;
+    });
+}
+
 
 
 /* (Server event) Server webSockets event 'check_status_response' */
@@ -70,6 +123,7 @@ function handleServerStatusEvent() {
         document.getElementById('result').innerHTML = `(${day}/${month}/${year}) - ${hour*3600 + 60*minute + second}'' - (simple-books-api) Status: ${data}<br>`;
     });
 }
+
 
 /* (Server event) Server webSockets event 'bookList_response' */
 function handleServerBookListEvent() {
